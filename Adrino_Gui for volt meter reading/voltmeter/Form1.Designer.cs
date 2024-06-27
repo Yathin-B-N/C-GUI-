b2fe167e532328479c01e6c46f23d6ea8258b5ca
Yathin-B-N <116481046+Yathin-B-N@users.noreply.github.com>
2024-06-27 09:33:37 +0530

    Add files via upload
    Retrieves the Voltage and resistance value from varying the resistance in Potentiometer through Adrino board and displayes in GUI C#

---
+++ Adrino_Gui for volt meter reading/voltmeter/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace voltmeter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.portcmb = new System.Windows.Forms.ComboBox();
+            this.baudcmb = new System.Windows.Forms.ComboBox();
+            this.volLbl = new System.Windows.Forms.Label();
+            this.openbtn = new System.Windows.Forms.Button();
+            this.closedbtn = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.resval = new System.Windows.Forms.Label();
+            this.currval = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PORT NO";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // portcmb
+            // 
+            this.portcmb.FormattingEnabled = true;
+            this.portcmb.Items.AddRange(new object[] {
+            "COM12",
+            "COM6",
+            "COM11",
+            "COM5"});
+            this.portcmb.Location = new System.Drawing.Point(199, 13);
+            this.portcmb.Name = "portcmb";
+            this.portcmb.Size = new System.Drawing.Size(121, 24);
+            this.portcmb.TabIndex = 2;
+            this.portcmb.SelectedIndexChanged += new System.EventHandler(this.portcmb_SelectedIndexChanged);
+            // 
+            // baudcmb
+            // 
+            this.baudcmb.FormattingEnabled = true;
+            this.baudcmb.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "115200"});
+            this.baudcmb.Location = new System.Drawing.Point(199, 85);
+            this.baudcmb.Name = "baudcmb";
+            this.baudcmb.Size = new System.Drawing.Size(121, 24);
+            this.baudcmb.TabIndex = 3;
+            this.baudcmb.SelectedIndexChanged += new System.EventHandler(this.baudcmb_SelectedIndexChanged);
+            // 
+            // volLbl
+            // 
+            this.volLbl.AutoSize = true;
+            this.volLbl.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.volLbl.Location = new System.Drawing.Point(53, 261);
+            this.volLbl.Name = "volLbl";
+            this.volLbl.Size = new System.Drawing.Size(150, 58);
+            this.volLbl.TabIndex = 4;
+            this.volLbl.Text = "0.00V";
+            this.volLbl.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // openbtn
+            // 
+            this.openbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openbtn.Location = new System.Drawing.Point(358, 13);
+            this.openbtn.Name = "openbtn";
+            this.openbtn.Size = new System.Drawing.Size(107, 27);
+            this.openbtn.TabIndex = 5;
+            this.openbtn.Text = "OPEN";
+            this.openbtn.UseVisualStyleBackColor = true;
+            this.openbtn.Click += new System.EventHandler(this.openbtn_Click);
+            // 
+            // closedbtn
+            // 
+            this.closedbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closedbtn.Location = new System.Drawing.Point(358, 82);
+            this.closedbtn.Name = "closedbtn";
+            this.closedbtn.Size = new System.Drawing.Size(107, 27);
+            this.closedbtn.TabIndex = 6;
+            this.closedbtn.Text = "Closed";
+            this.closedbtn.UseVisualStyleBackColor = true;
+            this.closedbtn.Click += new System.EventHandler(this.closedbtn_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // resval
+            // 
+            this.resval.AutoSize = true;
+            this.resval.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resval.Location = new System.Drawing.Point(247, 261);
+            this.resval.Name = "resval";
+            this.resval.Size = new System.Drawing.Size(248, 58);
+            this.resval.TabIndex = 7;
+            this.resval.Text = "0.00Ohms";
+            this.resval.Click += new System.EventHandler(this.resval_Click);
+            // 
+            // currval
+            // 
+            this.currval.AutoSize = true;
+            this.currval.Font = new System.Drawing.Font("Microsoft YaHei UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currval.Location = new System.Drawing.Point(584, 261);
+            this.currval.Name = "currval";
+            this.currval.Size = new System.Drawing.Size(152, 58);
+            this.currval.TabIndex = 8;
+            this.currval.Text = "0.00A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.currval);
+            this.Controls.Add(this.resval);
+            this.Controls.Add(this.closedbtn);
+            this.Controls.Add(this.openbtn);
+            this.Controls.Add(this.volLbl);
+            this.Controls.Add(this.baudcmb);
+            this.Controls.Add(this.portcmb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox portcmb;
+        private System.Windows.Forms.ComboBox baudcmb;
+        private System.Windows.Forms.Label volLbl;
+        private System.Windows.Forms.Button openbtn;
+        private System.Windows.Forms.Button closedbtn;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label resval;
+        private System.Windows.Forms.Label currval;
+    }
+}
+
---
+++ Adrino_Gui for volt meter reading/voltmeter/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace voltmeter
+{
+    public partial class Form1 : Form
+    {
+        String ArduData = "";
+        String Ae = "";
+        String be = "";
+        String c = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void portcmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void baudcmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = portcmb.Text;
+                serialPort1.BaudRate = Convert.ToInt32(baudcmb.Text);
+                serialPort1.Open();
+                openbtn.Enabled = false;
+                closedbtn.Enabled = true;
+                timer1.Enabled = true;
+                
+            }
+            catch(Exception errName)
+            {
+                MessageBox.Show(errName.ToString());
+            }
+        }
+
+        private void closedbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = portcmb.Text;
+                serialPort1.BaudRate = Convert.ToInt32(baudcmb.Text);
+                serialPort1.Close();
+                openbtn.Enabled = true;
+                closedbtn.Enabled = false;
+                timer1.Enabled = false;
+
+
+            }
+            catch (Exception errName)
+            {
+                MessageBox.Show(errName.ToString());
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            ArduData = serialPort1.ReadLine();
+            Ae = ArduData.Substring(0,5);
+            be = ArduData.Substring(5,7);
+            c = ArduData.Substring(12);
+          }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            volLbl.Text = Ae;
+            resval.Text = be;
+            currval.Text = c;
+        }
+
+        private void resval_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
